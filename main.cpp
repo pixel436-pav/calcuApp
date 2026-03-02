eeb3cfea11e85378135c309c2bfe21606540bddf
pavneetSingh <pavneets436@gmail.com>
2026-03-03 02:13:02 +0530

    Functions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 // this is the calculator app in cpp just to get familiar with syntax - build
 // using switch statement
+// cpp reads code from top to bottom if a function is defined after the main function then the main function will not know that it exists 
 #include <iostream>
 using namespace std;
+
+double add(double a ,double b){
+    
+    return a + b;
+}
+double product(double a , double b){
+    return a * b;
+}
+double sub(double a , double b ){
+    return a - b;
+}
+double divide(double a , double b ){
+    if(b == 0){
+        cout << "Error! cannot divide by zero" << endl;
+        return 0;
+    }
+    return a / b ;
+}
 int main() {
   double num1, num2;
   char op;
   char choice;
+  
 
   do{cout << "Enter Your First number : ";
   cin >> num1;
@@ -16,26 +36,24 @@ int main() {
 
   switch (op) {
   case '+':
-    cout << num1 + num2 << endl;
+    cout << add(num1,num2 ) << endl;
     break;
   case '-':
-    cout << num1 - num2 << endl;
+    cout << sub(num1, num2)<< endl;
     break;
   case '*':
-    cout << num1 * num2 << endl;
+    cout << product(num1, num2) << endl;
     break;
   case '/':
-    if (num2 == 0) {
-      cout << "Error! Cannot divide by 0";
-    } else {
-      cout << num1 / num2 << endl;
-    }
+   
+      cout << divide(num1, num2) << endl;
+    
     break;
   default:
     cout << "Invalid operator";
   }
     
-    cout << "\nCalculate Again(y/n): ";
+    cout << "Calculate Again(y/n): ";
     cin >> choice;
   }while(choice =='y');
   
@@ -43,3 +61,5 @@ int main() {
 
   return 0;
 }
+
+
