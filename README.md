6f446b1377c94329e8eec31522aaed125c8de29a
pavneetSingh <pavneets436@gmail.com>
2026-03-03 01:12:15 +0530

    Hello World

---
+++ README.md
@@ -0,0 +1,4 @@
+# Calculator - App 
+
+These are just simple calculator apps build to learn cpp syntax 
+
---
+++ main.cpp
@@ -0,0 +1,11 @@
+// this is the calculator app in cpp just to get familiar with syntax - build using switch statement 
+#include <iostream>
+using namespace std;
+ int main () {
+     double num1, num2 ;
+     char op;
+     
+     cout << "hello World" << endl;
+     
+     return 0;
+ }
