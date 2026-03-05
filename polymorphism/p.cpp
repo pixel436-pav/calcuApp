239d1930b9712dc32e4c51bdf71644776fdd097b
pavneetSingh <pavneets436@gmail.com>
2026-03-06 00:29:42 +0530

    Polymorphism

---
+++ polymorphism/p.cpp
@@ -0,0 +1,14 @@
+// Polymorphism - Multiple forms of one thing , One thing that is playing more than one role , ONE name multiple forms eg: Funtion Overloading
+// Compile time Polymorphism - object is bound by function call (also called early binding) done with the help of operator over loading , function OverLoading
+// Run time Polymorphism - can be achieved using virtual polymorphism , which function will be called decision is postponed (differ) - basically at the time of execution not even at the time of running - actually when the programming is running it is decided at the moment 
+
+#include <iostream>
+using namespace std;
+
+
+
+int main (){
+    
+    cout << "Polymorphism" << endl;
+    
+    return 0;}
